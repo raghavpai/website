2b1d4247f6691e76071f0089395395062aef7c1b
Raghavendra Pai <raghavendpai@gmail.com>
2024-06-29 21:24:23 +0530

    update
    update

---
+++ composeApp.d.ts
@@ -0,0 +1,4 @@
+type Nullable<T> = T | null | undefined
+export declare function addNumbers(a: number, b: number): number;
+export declare function greet(name: string): string;
+export declare function main(): void;
