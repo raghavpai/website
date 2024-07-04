7212017259bdbed2f9da49a4b27e3436c475cbd2
Raghavendra Pai <raghavendpai@gmail.com>
2024-07-04 17:40:05 +0530

    update
    update

--- composeApp.d.ts
+++
@@ -1,5 +0,0 @@
-type Nullable<T> = T | null | undefined
-export declare function main(): void;
-export declare function connectionLost(message: Nullable<string>): void;
-export declare function messageArrived(topic: Nullable<string>, message: Nullable<string>): void;
-export declare function connectComplete(): void;
